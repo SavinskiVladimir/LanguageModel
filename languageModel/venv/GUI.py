4254ca8e95e316f225c0674338fc475059f656bd
Савинский Владимир <vovka_savinskiy@mail.ru>
2025-04-03 18:38:59 +0300

    Настройка интерфейса, определение методов для связи интерфейса с моделью

--- languageModel/venv/GUI.py
+++ languageModel/venv/GUI.py
@@ -2,19 +2,27 @@ import tkinter as tk
 from tkinter import scrolledtext
 
 def request(request_text):
-    return "Ответ"
+    return request_text
 
-def get_request_text():
-    pass
+def get_request_text(text_entry, response_scrolled):
+    text = text_entry.get("1.0", "end")
+    response_scrolled.delete("1.0", "end")
+    response_scrolled.insert(tk.END, request(text))
+
 
 def main():
     window = tk.Tk()
     window.title("Языковая модель")
     window.geometry("800x600")
-    window.config(bg="white")
+    window.config(bg="#91D9A3")
+
+    # окно для вывода ответа
+    response_srcoled = scrolledtext.ScrolledText(window, width=60, height=11, font=("Calibri", 14))
+    response_srcoled.place(x=160, y=15)
 
     # надпись приглашение ко вводу
     text_entry_label = tk.Label(window, text="Введите ключевую фразу для генерации текста", font=("Calibri", 14))
+    text_entry_label.config(bg="#91D9A3")
     text_entry_label.place(x=215, y=280)
 
     # поле для ввода
@@ -22,7 +30,8 @@ def main():
     text_entry.place(x=160, y=320)
 
     # кнопка для ввода текста
-    text_entry_button = tk.Button(window, text="Получить результат", width=30, height=1, font=("Calibri", 14), borderwidth=2, relief="raised", activebackground="blue", command=lambda: get_request_text())
+    text_entry_button = tk.Button(window, text="Получить результат", width=30, height=1, font=("Calibri", 14), borderwidth=1, relief="sunken", activebackground="#C8CFC9", command=lambda: get_request_text(text_entry, response_srcoled))
+    text_entry_button.config(bg="#DAE3DB")
     text_entry_button.place(x=245, y=520)
 
     # логотип
@@ -31,10 +40,5 @@ def main():
     logo_label = tk.Label(window, image=logo_image)
     logo_label.place(x=10, y=10)
 
-
-    # окно для вывода ответа
-    response_srcoled = scrolledtext.ScrolledText(window, width=60, height=11, font=("Calibri", 14))
-    response_srcoled.insert(tk.END, request("Запрос"))
-    response_srcoled.place(x=160, y=15)
 
     window.mainloop()
